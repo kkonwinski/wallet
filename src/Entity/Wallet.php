56ac3858bb50afada5cbc019c06b29b4009fe3cd
Krzysztof Konwiński <k.konwinski@josera.pl>
2022-01-20 13:32:49 +0100

    add wallet entity

---
+++ src/Entity/Wallet.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WalletRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=WalletRepository::class)
+ */
+class Wallet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer", options={"default" : 0})
+     */
+    private $balance;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBalance(): ?int
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(?int $balance): self
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+}
